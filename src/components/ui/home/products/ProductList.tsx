18c3d08c4bf59fda7c723468f55a49b864507321
rcw108 <rcw108.m@gmail.com>
2024-08-09 20:33:41 +0300

    server action view

--- src/components/ui/home/products/ProductList.tsx
+++ src/components/ui/home/products/ProductList.tsx
@@ -2,6 +2,7 @@
 import { useActions } from '@/hooks/useActions'
 import { useProducts } from '@/hooks/useProducts'
 import { WooCommerceSingleProduct } from '@/types/wooCommerce.interface'
+import Link from 'next/link'
 import { FC, useEffect } from 'react'
 
 interface ProductListProps {
@@ -18,14 +19,14 @@ const ProductList: FC<ProductListProps> = ({ products }) => {
 
 	return (
 		<div>
-			{/* {products.map(product => {
+			{products.map(product => {
 				if (product.status !== 'publish') return
 				return (
 					<div key={product.id}>
 						<Link href={`/products/${product.slug}`}>{product.name}</Link>
 					</div>
 				)
-			})} */}
+			})}
 		</div>
 	)
 }
